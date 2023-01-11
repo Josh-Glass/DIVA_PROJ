37608e26dd9f98d9b72a03bb4a0c9a823ac3db35
Joshua Glass <apersonnamedjosh@gmail.com>
2023-01-11 15:09:49 -0500

    narrowed the search space to space = {     "learn_rate": {         "range": [1.9, 2.9],         "type": "Real"     },     "num_hidden_nodes": {         "range": [8, 12],         "type": "Integer"     },     "weight_range": {         "range": [1.5, 2.5],         "type": "Real"     },     "beta": {         "range": [400.0, 500.0],         "type": "Integer"     },     "c": {         "range": [400.0, 500.0],         "type": "Integer"     },
    }
    	modified:   search.py

--- search.py
+++ search.py
@@ -10,23 +10,23 @@ tested_model = 'diva_shj'
 
 space = {
     "learn_rate": {
-        "range": [0.1, 5],
+        "range": [1.9, 2.9],
         "type": "Real"
     },
     "num_hidden_nodes": {
-        "range": [1, 25],
+        "range": [8, 12],
         "type": "Integer"
     },
     "weight_range": {
-        "range": [.5, 5.5],
+        "range": [1.5, 2.5],
         "type": "Real"
     },
     "beta": {
-        "range": [0.0, 500.0],
+        "range": [400.0, 500.0],
         "type": "Integer"
     },
     "c": {
-        "range": [0.0, 500.0],
+        "range": [400.0, 500.0],
         "type": "Integer"
     },
     
@@ -37,7 +37,7 @@ space = {
 print('Search Started')
 
 objective_func = divaWrap_RespProbMethod_nosofskySuggestion.get_fit
-best_params = simutils.skopt_search(objective_func=objective_func, space=space, iters= 1000, inits=1000, plot_results=False)
+best_params = simutils.skopt_search(objective_func=objective_func, space=space, iters= 1500, inits=1500, plot_results=False)
 
 
 print('Search Complete!')
